9ccdf991fcb2e89ba5eb26e1a6864dc50fad4a65
Raheem <umar020820@gmail.com>
2024-09-16 08:22:08 -0700

    first commit js

---
+++ js/script.js
@@ -0,0 +1,185 @@
+"use strict";
+
+// alert('Hello');
+
+// const result = confirm('Are you here?');
+// console.log(result);
+
+// const answer = +prompt("Vam est 18?", "18");
+// console.log(answer + 5);
+
+
+// const answers = [];
+
+// answers[0] = prompt('Kak vawe imya?', '');
+// answers[1] = prompt('Kak vawe familiya?', '');
+// answers[2] = prompt('skolko vam let?', '');
+
+// document.write(answers);
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+
+const user = 'Ivan';
+
+alert(`привет, ${user}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////////////////////////////////////////////////////////////////////
+
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// const obj = {
+//     a: 50
+// };
+
+// obj.a = 11;
+// console.log(obj);
+
+// console.log(name);
+// var name = 'Raheem';
+
+
+
+//snake_case
+//UPPER_SNAKE_CASE
+//Kebab-case
+//PascalCase
+
+
+// let number = 5.5;
+
+// console.log(5/0);
+// console.log('string' * 9);
+
+// const persone = 'Alex'
+
+// const bool = true;
+
+// console.log(something);
+
+// let und;
+
+// console.log(und);
+
+// const obj = {
+//     name: 'John',
+//     age: 25, 
+//     isMarried: false
+// }
+
+// console.log(obj.age);
+// console.log(obj['name']);
+
+// let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+
+// console.log(arr[1]);
+
+// const arr = ['a', 'b', 'c']; // Перечень информации по порядку!!!!
+// const arrObj = {
+//     0: 'a',
+//     1: 'b',
+//     2: 'c'
+// }
+
+// // arrObj.b = '1234'
+// arrObj['b'] = '1234'
+
+
+// // console.log(arr[1]);
+// console.log(arrObj['b']);
+// console.log(arrObj.b);
+
+
+
+
+
+
+
+// const obj = {a: 1, b: 2};
+// const obj = {
+//      Anna: 500, 
+//     'Alice': 800
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
